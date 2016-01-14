f2aad41f4a884cf89922ef46c3d49ba55a3e858b
Craig Patten <cjp@cjp.id.au>
2016-01-14 23:06:01 +1030

    +boto

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ import glob
 from setuptools import setup
 
 setup(name="ersa-reporting",
-      version="1.2.2",
-      install_requires=["flask>=0.10.1", "flask-restful", "flask-cors",
+      version="1.3.0",
+      install_requires=["flask>=0.10.1", "flask-restful", "flask-cors", "boto",
                         "flask-sqlalchemy", "psycopg2", "requests", "arrow"],
       packages=["ersa_reporting"],
       scripts=glob.glob("bin/ersa-reporting-*"))
